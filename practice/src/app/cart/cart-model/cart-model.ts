f4f826b99df6c0cc08c8aaacab0bda9bba8f1f58
ibtisam2022skipq <106611734+ibtisam2022skipq@users.noreply.github.com>
2022-08-22 07:01:22 -0400

    Delete practice directory

--- practice/src/app/cart/cart-model/cart-model.ts
+++
@@ -1,8 +0,0 @@
-export class CartModel {
-    constructor(
-      public productName: String = '',
-      public productPrice: Number = 0,
-      public isAvailable: boolean = false
-    ) {}
-  }
-  
